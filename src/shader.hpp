b81ee5ca9456f37d6440c62b828fe1bcf865799a
Thomas Johnson <thomas27johnson39@gmail.com>
2025-03-02 00:19:35 +0000

    Add ShaderStage class.

--- src/shader.hpp
+++ src/shader.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
+#include <string>
 #include <filesystem>
 
 #include <glad/glad.h>
+
+
+/**
+ * Shader Stage class for storing an individual shader stage.
+ * Compiles and creates a shader of given type. Deletes shader
+ * upon going out of scope.
+ */
+class ShaderStage {
+public:
+	ShaderStage(const std::filesystem::path& sourcePath, GLenum type);
+	~ShaderStage();
+
+	inline GLenum GetShaderType() const { return m_type; }
+	inline GLuint GetShader() const { return m_shader; }
+
+protected:
+	std::string LoadSource(const std::filesystem::path& sourcePath);
+	void CompileShader(const std::filesystem::path& sourcePath);
+
+private:
+	GLenum m_type;
+	GLuint m_shader;
+};
+
 
 class Shader {
 public:
@@ -11,14 +36,13 @@ public:
 
 	/**
 	 * @brief Activate the shader program for use in OpenGL draw calls.
+	 * 
+	 * Marked as const as it doesn't change object state but it does change
+	 * OpenGL state.
 	 */
-	inline void BindShader() { glUseProgram(m_shaderProgram); }
-
+	inline void BindShader() const { glUseProgram(m_shaderProgram); }
 
 protected:
-	void LoadSource(const std::filesystem::path& sourcePath);
-	void CompileShader(const std::filesystem::path& sourcePath);
-
 	void MakeProgram();
 
 private:
