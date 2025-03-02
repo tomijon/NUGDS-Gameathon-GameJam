ba73874d40bcb5c6261f9da76c089b195f7602ff
Thomas Johnson <thomas27johnson39@gmail.com>
2025-03-02 00:10:24 +0000

    Add shader class declaration.

--- src/shader.hpp
+++ src/shader.hpp
@@ -1,1 +1,26 @@
 #pragma once
+
+#include <filesystem>
+
+#include <glad/glad.h>
+
+class Shader {
+public:
+	Shader(const std::filesystem::path& vertexPath, const std::filesystem::path& fragmentPath);
+	~Shader();
+
+	/**
+	 * @brief Activate the shader program for use in OpenGL draw calls.
+	 */
+	inline void BindShader() { glUseProgram(m_shaderProgram); }
+
+
+protected:
+	void LoadSource(const std::filesystem::path& sourcePath);
+	void CompileShader(const std::filesystem::path& sourcePath);
+
+	void MakeProgram();
+
+private:
+	GLuint m_shaderProgram;
+};
